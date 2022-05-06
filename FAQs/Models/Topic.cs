fcb389d50f7296680fc066fe5bb01c2351490aab
asbro <asbro@LAPTOP-110S6413>
2022-05-05 21:25:31 -0400

    Add project files.

---
+++ FAQs/Models/Topic.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FAQs.Models
+{
+    public class Topic
+    {
+        // primary key must be user friendly in a URL
+        public string TopicId { get; set; }
+        public string TName { get; set; }
+
+
+    }
+}
